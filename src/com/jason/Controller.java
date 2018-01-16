159b0dff54bb6d884e867488e01bdb675c4ed29f
xiaozhen xue <xxuettu@gmail.com>
2018-01-15 22:09:57 -0500

    add another controller

--- src/com/jason/Controller.java
+++ src/com/jason/Controller.java
@@ -2,13 +2,21 @@ package com.jason;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
-@RequestMapping(method = RequestMethod.GET, value = "/")
-	public String uploadVideos(){
-		return "hello";
+	WelcomeService welcomeService = new WelcomeService();
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String welcome(){
+		return welcomeService.printHello();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/welcome")
+	public String welcomeWithName(@RequestParam(value = "name", defaultValue = "world") String name){
+		return welcomeService.printName(name);
 	}
 }
