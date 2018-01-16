f778ae2dc235462683398b92d27a41408af27058
xiaozhen xue <xxuettu@gmail.com>
2018-01-15 19:02:43 -0500

    add controller and main

---
+++ src/com/jason/Application.java
@@ -0,0 +1,15 @@
+package com.jason;
+
+
+import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+	
+    public static void main(String[] args) throws IOException, Exception {    	
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/com/jason/Controller.java
@@ -0,0 +1,14 @@
+package com.jason;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String uploadVideos(){
+		return "";
+	}
+}
