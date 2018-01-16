72049af91d53f71042f6068ab1f313f3c875ef47
YINXIA LI <nmbmlyx0211@YINXIAs-Air.fios-router.home>
2018-01-15 21:47:29 -0500

    add welcomeService file

---
+++ src/com/jason/WelcomeService.java
@@ -0,0 +1,11 @@
+package com.jason;
+
+public class WelcomeService {
+	public String printHello(){
+		return "hello world";
+	}
+	public String printName(String name){
+		return "hello " + name;
+	}
+
+}
